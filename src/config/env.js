d04971a27fba3936300323dc9611e2e94a7c6c04
Oladele Sunday O. <sundayoladele2@gmail.com>
2026-03-25 20:23:33 +0100

    cors ammendment

--- src/config/env.js
+++ src/config/env.js
@@ -4,7 +4,7 @@ export const configuration = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_URL: process.env.API_URL || 'http://localhost:5000',
     FRONTEND_URL: process.env.FRONTEND_URL || 'https://volunteer-bridge.com.ng',
-    ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN ? String(process.env.ALLOWED_ORIGIN).split(',') : ['http://localhost:5500', 'https://volunteer-bridge.com.ng'],
+    ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN ? String(process.env.ALLOWED_ORIGIN).split(',') : ['http://127.0.0.1:5500', 'https://volunteer-bridge.com.ng'],
     PORT: process.env.PORT || 5000,
     DB_HOST: process.env.DB_HOST || 'localhost',
     DB_USER: process.env.DB_USER || 'volunteer_user',
