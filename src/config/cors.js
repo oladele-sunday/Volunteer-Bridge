257c5dec1443691c7c00bd6ded3ea50c194429b9
LEad <136821834+Lyowika7@users.noreply.github.com>
2026-03-31 17:38:07 +0100

    Simplify CORS options configuration

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,27 +1,5 @@
-import { configuration } from "./env.js";
-
-const allowedOrigins = [
-  ...(configuration.ALLOWED_ORIGIN
-    ? configuration.ALLOWED_ORIGIN.split(",").map((origin) => origin.trim())
-    : []),
-  "http://localhost:8080",
-  "http://127.0.0.1:8080",
-  "http://localhost:5173",
-  "http://127.0.0.1:5173",
-].filter(Boolean);
-
 export const corsOptions = {
-  origin(origin, callback) {
-    console.log("Origin:", origin);
-    console.log("Allowed Origins:", allowedOrigins);
-
-    if (!origin || allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-
-    return callback(null, false);
-  },
-
+  origin: true,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
