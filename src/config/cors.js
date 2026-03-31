536437ca9b5dd5b65f939d776ff6480d00d7d219
Oladele Sunday <sundayoladele2@gmail.com>
2026-03-31 21:55:56 +0100

    cors ammended and port

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,6 +1,15 @@
+import { configuration } from './env.js';
+
 export const corsOptions = {
-  origin: true,
+  origin(origin, callback) {
+    if (!origin || configuration.ALLOWED_ORIGIN.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error("Not allowed by CORS"));
+  },
+
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 };
