f812b4a8967c5c5cf84423b9589c6d40848f3bf9
LEad <136821834+Lyowika7@users.noreply.github.com>
2026-04-02 15:05:05 +0100

    Add route for retrieving user's tasks

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,15 +1,16 @@
 import express from "express";
+import authMiddleware from "../middleware/auth.js";
 import {
   getProjectTasks,
+  getMyTasks,
   createTask,
   assignTask,
   updateTaskStatus,
 } from "../controllers/task.controller.js";
-import authMiddleware from "../middleware/auth.js";
-// import role from "../middleware/role.js";
 
 const router = express.Router();
 
+router.get("/my-tasks", authMiddleware, getMyTasks);
 router.get("/projects/:projectId/tasks", authMiddleware, getProjectTasks);
 router.post("/projects/:projectId/tasks", authMiddleware, createTask);
 router.post("/:id/assign", authMiddleware, assignTask);
